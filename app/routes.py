4fe3d1a80838d650be86ef835eac25e8d335cf49
Joe <jreuwood@gmail.com>
2020-08-22 17:31:19 +0100

    deleted obsolete main.css, main.js

--- app/routes.py
+++ app/routes.py
@@ -8,25 +8,7 @@ from decimal import Decimal
 
 @app.route('/', methods=['GET', 'POST'])
 def landing_page():
-
     booking_form = BookingForm()
-
-    if booking_form.validate_on_submit():
-        customer = Customer(name=booking_form.name.data, email_address=booking_form.email_address.data, phone_number=booking_form.phone_number.data)
-        booking = Booking(confirmed=False, paid=False, cancelled=False)
-
-        customer.bookings.append(booking)
-
-        date_delta = (booking_form.departure_date.data - booking_form.arrival_date.data).days
-        for i in range(0, date_delta+1):
-            date = Date(date=(booking_form.arrival_date.data + timedelta(days=i)).isoformat())
-            booking.dates.append(date)
-            db.session.add(date)
-
-        db.session.add(customer)
-        db.session.add(booking)
-        db.session.commit()
-
     return render_template('index.html', booking_form=booking_form)
 
 @app.route('/dates', methods=['POST'])
