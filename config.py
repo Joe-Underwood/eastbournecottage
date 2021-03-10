b20c0caabbbf1cef7abfa8ca0f54e67e216534c7
Joe <jreuwood@gmail.com>
2021-03-10 12:01:34 +0000

    removed flask server_name

--- config.py
+++ config.py
@@ -4,7 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config(object):
-    SERVER_NAME = os.getenv('SERVER_NAME')
     SECRET_KEY = os.getenv('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
